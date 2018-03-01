28d22fbf29091cc10b7988a27afd02dd701e83a7
nick <ssj00@163.com>
2018-03-01 22:48:33 +0800

    no message

---
+++ src/main/java/com/shang/zk/zookeeper/cluster/Client2.java
@@ -0,0 +1,11 @@
+package com.shang.zk.zookeeper.cluster;
+
+
+public class Client2 {
+
+	public static void main(String[] args) throws Exception{
+		
+		ZKWatcher myWatcher = new ZKWatcher();
+		Thread.sleep(100000000);
+	}
+}
